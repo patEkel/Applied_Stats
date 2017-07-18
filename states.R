99ca8d191cb29de94e9d5ba49ace24c6ad1ba11c
Patrick Ekel <patrick.ekel@utah.edu>
2017-07-18 08:33:15 -0600

    states and counties functionl. fixing whitespace and color hue

--- states.R
+++ states.R
@@ -1,35 +1,8 @@
 library(ggplot2)
 library(dplyr)
 library(readxl)
-us <- map_data("state") # get map for states
-arr <- USArrests 
-arr <- add_rownames(arr, "region") # name region column for states
-arr <- mutate(arr, region=tolower(region)) # make region column lowercase
-summary(arr)
-summary(us)
-head(arr)
-g <- ggplot()
-us
-g
-g <- g + geom_map(data = us, map = us, aes(x=long, y = lat, map_id=region),
-                  fill ="#ffffff", color ="#ffffff", size =.15)
-g
-g <- g + geom_map(data=arr, map=us,
-                    aes(fill=Murder, map_id=region),
-                    color="#ffffff", size=0.15)
-g
-g <- g + scale_fill_continuous(low='thistle2', high='darkred', 
-                                 guide='colorbar')
-g
-###########
-g <- g + labs(x=NULL, y=NULL) + 
-  coord_map("albers", lat0 = 39, lat1 = 45) + 
-  theme(panel.border = element_blank()) + 
-  theme(panel.background = element_blank()) + 
-  theme(axis.ticks = element_blank()) + 
-  theme(axis.text = element_blank())
-g
-##################################################################
+
+####################### STATE ###########################################
 up <- map_data("state")
 file <-read_excel(path='C:/Users/pat/Desktop/mortality_risk.xlsx')
 file <- file[c('state','value')]
@@ -45,3 +18,27 @@ g <- g+geom_map(data = up, map = up, aes(x=long, y=lat, map_id=region),
 g
 g <- g + geom_map(data=file, map=up, aes(fill=value, map_id=state), color="#ffffff", size=0.15)
 g
+###################### COUNTY #################################
+up <- map_data("county")
+state_map <-map_data("state")
+file <-read_excel(path='C:/Users/pat/Desktop/mortality_risk.xlsx')
+file <- file[c('region','subregion', 'life_expectancy')]
+file<-na.omit(file)#remove missing data
+head(file)
+up
+file$life_expectancy <- as.numeric(file$life_expectancy)
+file$life_expectancy
+g <- ggplot()
+g <- g+geom_map(data = up, map = up, aes(x=long, y=lat, map_id=region), 
+                fill="#ffffff", color="#ffffff", size=.15)
+
+g
+g <- g + geom_map(data=file, map=up, aes(fill=life_expectancy, map_id=subregion), color="#ffffff", size=0.15) +
+  labs(x=NULL, y=NULL) + 
+  coord_map("albers", lat0 = 39, lat1 = 45) + 
+  theme(panel.border = element_blank()) + 
+  theme(panel.background = element_blank()) + 
+  theme(axis.ticks = element_blank()) + 
+  theme(axis.text = element_blank())
+g
+  #geom_path(data = state_map , colour = "red", aes(x ="#ffffff", y ="#ffffff"))
